d6106dadd931cee275ce2515046dc499e9799ed4
Lenka Brandová <lenka_brandova@MacBook-Pro-uzivatela-Lenka.local>
2020-10-23 18:50:14 +0200

    Login/SignUp compoments created and navbar added to App.js file

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 
 import { HomePage } from 'src/pages/HomePage';
 import { PageNotFound } from 'src/pages/PageNotFound';
+import { RegistrationPage } from 'src/pages/RegistrationPage';
+import SignUp from "./components/signup.component";
 
 export const route = {
   home: () => `/`,
@@ -12,6 +14,8 @@ export function Routes() {
   return (
     <Switch>
       <Route path={route.home()} exact component={HomePage} />
+      <Route path="/registration" component={RegistrationPage} />
+      <Route path="/sign-up" component={SignUp} />
       <Route path="*" component={PageNotFound} />
     </Switch>
   );
