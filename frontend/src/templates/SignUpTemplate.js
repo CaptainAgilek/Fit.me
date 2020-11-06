8f91b541e9e38c1b77abfd4a0adc33d161a34303
Lenka Brandová <lenka_brandova@MacBook-Pro-uzivatela-Lenka.local>
2020-11-06 14:57:57 +0100

    React-bootstrap navbar added

--- frontend/src/templates/SignUpTemplate.js
+++ frontend/src/templates/SignUpTemplate.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-import { SignUpForm, TopNavigation} from 'src/organisms/';
+import { SignUpForm, Navigation} from 'src/organisms/';
 import { Row, Col, Container } from 'react-bootstrap';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
 
 export function SignUpTemplate({ isLoading, error, onSubmit }) {
   return (
     <>
-    <TopNavigation />
+     <Navbar bg="dark" variant="dark">
+       <Navbar.Brand href="/">Fit.me</Navbar.Brand>
+       <Nav className="mr-auto">
+        <Nav.Link href="/">Domov</Nav.Link>
+        <Nav.Link href="#features">O aplikaci</Nav.Link>
+      </Nav>
+      <Nav>
+         <Button variant="dark" href="/auth/signin">Přihlášení</Button>
+         <Button variant="dark" href="/auth/signup">Registrace</Button>
+     </Nav>
+     </Navbar>
+
     <Container>
       <Row className="justify-content-md-center">
         <h1>Registrace</h1>
