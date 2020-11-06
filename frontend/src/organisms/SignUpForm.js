ad2e953cc6971be3d06d7599d5e6acceb6cc5b5f
Lenka Brandová <lenka_brandova@MacBook-Pro-uzivatela-Lenka.local>
2020-11-06 12:34:08 +0100

    Correction of SignIn, SignUp validation

--- frontend/src/organisms/SignUpForm.js
+++ frontend/src/organisms/SignUpForm.js
@@ -19,7 +19,7 @@ const schema = yup.object().shape({
   username: yup.string().required('Vyplňte username'),
   firstname: yup.string().required('Vyplňte jméno'),
   lastname: yup.string().required('Vyplňte příjmení'),
-  email: yup.string().email().required('Vyplňte email'),
+  email: yup.string().required('Vyplňte email').matches(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i, 'Prosím zadajte email v správním tvaru'),
   password: yup.string().required('Vyplňte heslo').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, 'Musí obsahovat 8 znaků, alespoň jeden velký a malý znak a číslo'),
   passwordConfirmation: yup
     .string()
@@ -77,42 +77,42 @@ export function SignUpForm({
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <Field name="username" type="text"
-                   className={'form-control' + (errors.username || touched.username ? ' is-invalid' : '')} />
+                   className={'form-control' + (errors.username ? ' is-invalid' : '')} />
             <ErrorMessage name="username" component="div" className="invalid-feedback" />
           </div>
 
           <div className="form-group">
             <label htmlFor="firstname">Jméno</label>
             <Field name="firstname" type="text"
-                   className={'form-control' + (errors.firstname || touched.firstname ? ' is-invalid' : '')} />
+                   className={'form-control' + (errors.firstname ? ' is-invalid' : '')} />
             <ErrorMessage name="firstname" component="div" className="invalid-feedback" />
           </div>
 
           <div className="form-group">
             <label htmlFor="lastname">Příjmení</label>
             <Field name="lastname" type="text"
-                   className={'form-control' + (errors.lastname && touched.lastname ? ' is-invalid' : '')} />
+                   className={'form-control' + (errors.lastname ? ' is-invalid' : '')} />
             <ErrorMessage name="lastname" component="div" className="invalid-feedback" />
           </div>
 
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <Field name="email" type="email"
-                   className={'form-control' + (errors.email || touched.email ? ' is-invalid' : '')} />
+                   className={'form-control' + (errors.email ? ' is-invalid' : '')} />
             <ErrorMessage name="email" component="div" className="invalid-feedback" />
           </div>
 
           <div className="form-group">
             <label htmlFor="password">Heslo</label>
             <Field name="password" type="password"
-                   className={'form-control' + (errors.password || touched.password ? ' is-invalid' : '')} />
+                   className={'form-control' + (errors.password ? ' is-invalid' : '')} />
             <ErrorMessage name="password" component="div" className="invalid-feedback" />
           </div>
 
           <div className="form-group">
             <label htmlFor="passwordConfirmation">Heslo znova</label>
             <Field name="passwordConfirmation" type="password"
-                   className={'form-control' + (errors.passwordConfirmation || touched.passwordConfirmation ? ' is-invalid' : '')} />
+                   className={'form-control' + (errors.passwordConfirmation ? ' is-invalid' : '')} />
             <ErrorMessage name="passwordConfirmation" component="div" className="invalid-feedback" />
           </div>
 
