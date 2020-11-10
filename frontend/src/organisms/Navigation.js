ca4f14ad6627e704e82639b15f117462e014f390
Martin <Kolomaznik.martin@outlook.com>
2020-11-10 17:18:06 +0100

    use react router in top navigation bar

--- frontend/src/organisms/Navigation.js
+++ frontend/src/organisms/Navigation.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Button from "react-bootstrap/Button";
 
-import { NavLink } from 'src/atoms/';
-
+import { Link } from 'react-router-dom';
 import { useAuth } from 'src/utils/auth';
 import { useHistory } from 'react-router-dom';
 import { route } from 'src/Routes';
@@ -15,40 +12,32 @@ export function Navigation() {
   const {user, signout } = useAuth();
   const history = useHistory();
 
+  const homeLink = route.home();
   const profileLink = route.userProfile();
+  const signInLink = route.signIn();
+  const signUpLink = route.signUp();
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      <Navbar.Brand href="/">Fit.me</Navbar.Brand>
+      <Navbar.Brand as={Link} to={homeLink}>Fit.me</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="mr-auto">
-       <Nav.Link href="/">Domov</Nav.Link>
-       <Nav.Link href="/about">O aplikaci</Nav.Link>
+        <Nav.Link as={Link} to="/about">O aplikaci</Nav.Link>
      </Nav>
      {user ? (
        <Nav>
-         <Button variant="light">
-           <NavLink exact to={profileLink}>
-            Profil
-          </NavLink>
-        </Button>
-
-        {/*<Button variant="light">
-          <NavLink onClick={() => {
-              signout();
-              history.push(route.home());
-              window.location.reload();
-            }}
-          >
-            Odhlásit se
-          </NavLink>
-        </Button>*/}
+        <Nav.Link as={Link} to={profileLink}>Profil</Nav.Link>
+        <Nav.Link onClick={() => {
+            signout();
+            history.push(route.home());
+            window.location.reload();
+          }}>Odhlásit se</Nav.Link>
        </Nav>
       ) : (
         <Nav>
-          <Button variant="light" href="/auth/signin">Přihlášení</Button>
-          <Button variant="light" href="/auth/signup">Registrace</Button>
+          <Nav.Link as={Link} to={signInLink}>Přihlášení</Nav.Link>
+          <Nav.Link as={Link} to={signUpLink}>Registrace</Nav.Link>
         </Nav>
       )
      }
