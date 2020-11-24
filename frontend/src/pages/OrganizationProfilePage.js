e012f86c32eab77c8555ec02358609b0122ea12c
Petr Šimandl <simp06@vse.cz>
2020-11-25 00:40:01 +0100

    add taking user object from auth

--- frontend/src/pages/OrganizationProfilePage.js
+++ frontend/src/pages/OrganizationProfilePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { OrganizationProfileTemplate } from 'src/templates/OrganizationProfileTemplate';
 import { gql, useMutation, useQuery } from '@apollo/client';
+import { useAuth } from 'src/utils/auth';
 
 export function OrganizationProfilePage() {
-  const user = { user_id: 104 }; //todo
-
+  //const user = { user_id: 104 }; //todo
+  const { user } = useAuth();
   return (
     <>
       <OrganizationProfileTemplate user={user} />
