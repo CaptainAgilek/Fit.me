76587c2ffcc1c002f17cbc4ca5bf64a22c4814b2
jan.jaros <jan.jaros123@gmail.com>
2020-12-09 13:58:29 +0100

    FE part

--- frontend/src/organisms/OrganizationProfileServices.js
+++ frontend/src/organisms/OrganizationProfileServices.js
@@ -1,108 +1,110 @@
-import React, { useState } from "react";
-import { CategoryBox, Loading } from "../atoms";
-import { ErrorBanner } from "../molecules";
-import { Button, Container, Modal } from "react-bootstrap";
-import { gql, useMutation } from "@apollo/client";
+import React, { useState } from 'react';
+import { CategoryBox, Loading } from '../atoms';
+import { ErrorBanner } from '../molecules';
+import { Button, Container, Modal } from 'react-bootstrap';
+import { gql, useMutation } from '@apollo/client';
 
 export function OrganizationProfileServices({
-  servicesState,
-  organizationData,
-}) {
-  console.log("organization data ", JSON.stringify(organizationData));
+                                              servicesState,
+                                              organizationData,
+                                            }) {
+  console.log('organization data ', JSON.stringify(organizationData));
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = () => {
+    setShow(true);
+  };
 
   const categories = [
     {
       id: 6,
-      color: "#B9E0EE",
-      imgUrl: "/images/icons/aerialYoga.png",
+      color: '#B9E0EE',
+      imgUrl: '/images/icons/aerialYoga.png',
       children: <>aerial jóga</>,
     },
     {
       id: 7,
-      color: "#ACE2D8",
-      imgUrl: "/images/icons/gravidYoga.png",
+      color: '#ACE2D8',
+      imgUrl: '/images/icons/gravidYoga.png',
       children: <>gravid jóga</>,
     },
     {
       id: 8,
-      color: "#DBF6E9",
-      imgUrl: "/images/icons/hotYoga.png",
+      color: '#DBF6E9',
+      imgUrl: '/images/icons/hotYoga.png',
       children: <>hot jóga</>,
     },
     {
       id: 9,
-      color: "#FCF6D1",
-      imgUrl: "/images/icons/stralaYoga.png",
+      color: '#FCF6D1',
+      imgUrl: '/images/icons/stralaYoga.png',
       children: <>strala jóga</>,
     },
     {
       id: 10,
-      color: "#F6DEDA",
-      imgUrl: "/images/icons/jogaProZacatecniky.png",
+      color: '#F6DEDA',
+      imgUrl: '/images/icons/jogaProZacatecniky.png',
       children: <>jóga pro začátečníky</>,
     },
     {
       id: 11,
-      color: "#C16868",
-      imgUrl: "/images/icons/bodyPump.png",
+      color: '#C16868',
+      imgUrl: '/images/icons/bodyPump.png',
       children: <>body pump</>,
     },
     {
       id: 12,
-      color: "#ED8F8B",
-      imgUrl: "/images/icons/kravMaga.png",
+      color: '#ED8F8B',
+      imgUrl: '/images/icons/kravMaga.png',
       children: <>krav maga</>,
     },
     {
       id: 13,
-      color: "#EEACAC",
-      imgUrl: "/images/icons/thaiBox.png",
+      color: '#EEACAC',
+      imgUrl: '/images/icons/thaiBox.png',
       children: <>thai box</>,
     },
     {
       id: 14,
-      color: "#F8BB97",
-      imgUrl: "/images/icons/kruhovyTrenink.png",
+      color: '#F8BB97',
+      imgUrl: '/images/icons/kruhovyTrenink.png',
       children: <>kruhový trénink</>,
     },
     {
       id: 15,
-      color: "#F3D8C8",
-      imgUrl: "/images/icons/soukromeLekce.png",
+      color: '#F3D8C8',
+      imgUrl: '/images/icons/soukromeLekce.png',
       children: <>soukromé lekce</>,
     },
     {
       id: 16,
-      color: "#A4A0C3",
-      imgUrl: "/images/icons/sportovniAerobik.png",
+      color: '#A4A0C3',
+      imgUrl: '/images/icons/sportovniAerobik.png',
       children: <>sportovní aerobik</>,
     },
     {
       id: 17,
-      color: "#C3D2EB",
-      imgUrl: "/images/icons/pilates.png",
+      color: '#C3D2EB',
+      imgUrl: '/images/icons/pilates.png',
       children: <>pilates</>,
     },
     {
       id: 18,
-      color: "#ACDBEA",
-      imgUrl: "/images/icons/plavani.png",
+      color: '#ACDBEA',
+      imgUrl: '/images/icons/plavani.png',
       children: <>plavání</>,
     },
     {
       id: 19,
-      color: "#EAC0CE",
-      imgUrl: "/images/icons/poleDance.png",
+      color: '#EAC0CE',
+      imgUrl: '/images/icons/poleDance.png',
       children: <>pole dance</>,
     },
     {
       id: 20,
-      color: "#C7D4EC",
-      imgUrl: "/images/icons/zumba.png",
+      color: '#C7D4EC',
+      imgUrl: '/images/icons/zumba.png',
       children: <>zumba</>,
     },
   ];
@@ -140,7 +142,7 @@ export function OrganizationProfileServices({
       onError: (error) => {
         console.log(error);
       },
-    }
+    },
   );
 
   if (
@@ -150,10 +152,9 @@ export function OrganizationProfileServices({
     return <Loading />;
   }
 
-  //  console.log("place ",organizationData.organization.places[0].place_id);
-
   let tmpList = [];
   let servicesList = [];
+  let availableCategories = [];
   if (servicesState.data) {
     tmpList = servicesState.data.servicesForPlace;
 
@@ -172,12 +173,15 @@ export function OrganizationProfileServices({
         }
       });
     });
-    console.log(servicesList);
-  }
 
-  const handleClick = () => {
-    setShow(true);
-  };
+    categories.forEach((category) => {
+      if (servicesList.some((service) => service.service_id === category.id)) {
+        // availableCategories.push(category);
+      } else {
+        availableCategories.push(category);
+      }
+    });
+  }
 
   return (
     <Container>
@@ -188,47 +192,46 @@ export function OrganizationProfileServices({
 
       {/*List of services*/}
       {servicesState &&
-        servicesState.data &&
-        organizationData && (
-          <div className="d-flex justify-content-center align-items-start flex-wrap">
-            {servicesList.map((service) => {
-              return (
+      servicesState.data &&
+      organizationData && (
+        <div className="d-flex justify-content-center align-items-start flex-wrap">
+          {servicesList.map((service) => {
+            return (
+              <>
                 <CategoryBox
                   key={service.imageUrl}
                   color={service.color}
                   img={service.imageUrl}
                 >
-                  <button
-                    onClick={() =>
-                      deleteServiceRequest({
-                        variables: {
-                          place_id: service.place_id,
-                          service_id: service.service_id,
-                        },
-                      })
-                    }
+                  {service.children} <br />
+                  <button className="align-items-center"
+                          onClick={() =>
+                            deleteServiceRequest({
+                              variables: {
+                                place_id: service.place_id,
+                                service_id: service.service_id,
+                              },
+                            })
+                          }
                   >
                     SMAZAT
-                    {/*<img src="/images/icons/trash-alt-solid.svg" />*/}
                   </button>
-                  {service.children}
                 </CategoryBox>
-              );
-            })}
-            {/*add new*/}
-            <button onClick={handleClick}>
-              <div className="category-box">
-                <div className="category-box-img">
-                  <img
-                    className="img-fluid align-content-center"
-                    src={"/images/plus.png"}
-                    alt="Přidat službu"
-                  />
-                </div>
-              </div>
-            </button>
+
+
+              </>
+            );
+          })}
+          {/*add new*/}
+          <div className="category-box align-items-center" onClick={handleShow}>
+            <img
+              className="img-fluid align-content-center"
+              src={'/images/plus.png'}
+              alt="Přidat službu"
+            />
           </div>
-        )}
+        </div>
+      )}
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
@@ -236,7 +239,7 @@ export function OrganizationProfileServices({
         </Modal.Header>
         <Modal.Body>
           <div className="d-flex justify-content-center align-items-start flex-wrap">
-            {categories.map((category) => {
+            {availableCategories.map((category) => {
               return (
                 <div
                   onClick={() => {
@@ -245,7 +248,7 @@ export function OrganizationProfileServices({
                         input: {
                           // place_id: servicesState.servicesState.data.place_id,
                           place_id:
-                            organizationData.organization.places[0].place_id,
+                          organizationData.organization.places[0].place_id,
                           service_id: category.id,
                         },
                       },
