ed97aca90edd3414a3c95ce1f287f6f9514c212f
Lenka Brandová <lenka_brandova@MacBook-Pro-uzivatela-Lenka.local>
2020-12-12 18:48:28 +0100

    refactor

--- frontend/src/pages/TrainerProfilePage.js
+++ frontend/src/pages/TrainerProfilePage.js
@@ -51,6 +51,22 @@ const UPDATE_TRAINER_PROFILE_MUTATION = gql`
   }
 `;
 
+const CHANGE_PASSWORD_MUTATION = gql`
+  mutation changePassword(
+    $email: String!
+    $oldPassword: String!
+    $newPassword: String!
+    $newPasswordAgain: String!
+  ) {
+    changePassword(
+      email: $email
+      oldPassword: $oldPassword
+      newPassword: $newPassword
+      newPasswordAgain: $newPasswordAgain
+    )
+  }
+`;
+
 export function TrainerProfilePage() {
   const { user } = useAuth();
   const userId = user.user_id;
@@ -82,14 +98,39 @@ export function TrainerProfilePage() {
     }
   );
 
+  const [changePasswordRequest, changePasswordRequestState] = useMutation(
+    CHANGE_PASSWORD_MUTATION,
+    {
+      onCompleted: () => {
+        setActionSuccess({
+          message: "Heslo bylo změněno.",
+          variant: "success",
+        });
+      },
+    },
+    {
+      onError: () => {
+        setActionSuccess({
+          message: "Chyba při změně hesla.",
+          variant: "danger",
+        });
+      },
+    }
+  );
+
   return (
     <>
       <TrainerProfileTemplate
         trainerData={trainerFetcher.data}
         actionSuccess={actionSuccess}
         setActionSuccess={setActionSuccess}
-        error={trainerFetcher.error || updateTrainerRequestState.error}
+        error={
+          trainerFetcher.error ||
+          updateTrainerRequestState.error ||
+          changePasswordRequest.error
+        }
         updateTrainerRequest={updateTrainerRequest}
+        changePasswordRequest={changePasswordRequest}
       />
     </>
   );
