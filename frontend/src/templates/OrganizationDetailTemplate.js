ff934ae1aa734f4f4cbaba462e679d00dce76902
Petr Šimandl <simp06@vse.cz>
2020-12-30 14:31:36 +0100

    add rough trainers base

--- frontend/src/templates/OrganizationDetailTemplate.js
+++ frontend/src/templates/OrganizationDetailTemplate.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { Container, Row, Col, Carousel, Image } from "react-bootstrap";
+import { Container, Row, Col, Carousel, Image, ListGroup, Card } from "react-bootstrap";
 
 import {
     Footer,
@@ -150,12 +150,12 @@ export function OrganizationDetailTemplate({
                             </Col>
                         </Row>
 
-                        <Row className="organization-profile-section-container justify-content-center ">
+                        <Row className="justify-content-center ">
                             <Col lg={10} className="organization-detail-actions">
 
                                 <Row className="justify-content-center "><h2>Připravované akce</h2></Row>
 
-                                <Row className="justify-content-center "><Col lg={5}><hr class="organization-detail-hr"></hr></Col></Row>
+                                <Row className="justify-content-center "><Col lg={5}><hr class="organization-detail-actions-hr"></hr></Col></Row>
                                 <Row className="justify-content-center ">
                                     {console.log(actionsState, "aState")}
                                     {actionsState && actionsState.data &&
@@ -165,6 +165,44 @@ export function OrganizationDetailTemplate({
                                             actionsState={actionsState}
                                             editable={false}
                                             setActionSuccess={setActionSuccess} />}
+                                </Row>
+                            </Col>
+                        </Row>
+
+                        <Row className="organization-profile-section-container justify-content-center">
+                            <Col lg={10} className="organization-detail-trainers">
+                                <Row className="justify-content-center "><h2>Naši trenéři</h2></Row>
+                                <Row className="justify-content-center "><Col lg={5}><hr class="organization-detail-trainers-hr"></hr></Col></Row>
+
+                                <Row className="justify-content-center ">
+                                    <Col lg={6}>
+                                        <Row>
+                                            <Col lg={5} style={{ paddingRight: "0" }}>
+                                                <Image src={"http://dev.backend.team02.vse.handson.pro/photos/alonso-reyes-0HlI76m4jxU-unsplash-copy.jpg"} fluid></Image>
+                                            </Col>
+                                            <Col lg={7} style={{ paddingLeft: "0" }}>
+                                                <Card style={{ color: "#000000", height: "100%", width: "100%" }}>
+                                                    <Card.Body>
+                                                        <Card.Title>Tobias Reuter</Card.Title>
+                                                        {/*<ListGroup variant="flush">
+                                                            <ListGroup.Item>Celoživotní sportovec! Ke každému klientovi přistupuji individuálně. V první řadě projde klient diagnostikou pohybového aparátu a potom nastavíme přesný tréninkový proces, vzhledem k jeho cíle. Spolu si nadefinujeme pravidla které nás dovedou k vytouženému cíli.</ListGroup.Item>
+                                                            <ListGroup.Item>tobias.reuter@formfactory.cz</ListGroup.Item>
+                                                            <ListGroup.Item>tobias.reuter</ListGroup.Item>
+                                                        </ListGroup>*/}
+                                                        <Row style={{ fontSize: "13px", marginBottom: "1rem" }}>Celoživotní sportovec! Ke každému klientovi přistupuji individuálně. V první řadě projde klient diagnostikou pohybového aparátu a potom nastavíme přesný tréninkový proces, vzhledem k jeho cíle. Spolu si nadefinujeme pravidla které nás dovedou k vytouženému cíli.</Row>
+                                                        <Row>
+                                                            <Col lg={2}><Image src="/images/icons/facebook-f-brands.svg" fluid style={{ maxWidth: "3vh", maxHeight: "3vh" }}></Image></Col>
+                                                            <Col lg={10}>tobias.reuter@formfactory.cz</Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col lg={2}><Image src="/images/icons/instagram-square-brands.svg" fluid style={{ maxWidth: "3vh", maxHeight: "3vh" }}></Image></Col>
+                                                            <Col lg={10}>tobias.reuter</Col>
+                                                        </Row>
+                                                    </Card.Body>
+                                                </Card>
+                                            </Col>
+                                        </Row>
+                                    </Col>
                                 </Row>
                             </Col>
                         </Row>
