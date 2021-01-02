2bf3d33fed64766019970e2587447f58b91f86dd
Martin Sedlacek <sedlam39@fit.cvut.cz>
2021-01-02 20:19:35 +0100

    address search autocomplete

--- frontend/src/pages/SignedInUserLandingPage.js
+++ frontend/src/pages/SignedInUserLandingPage.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 import { SignedInUserLandingTemplate } from "src/templates/SignedInUserLandingTemplate";
+import { OpenStreetMapProvider } from "leaflet-geosearch";
 
 export function SignedInUserLandingPage() {
+  const mapProvider = new OpenStreetMapProvider({
+    params: {
+      addressdetails: 1,
+    },
+  });
+
   return (
     <>
-      <SignedInUserLandingTemplate />
+      <SignedInUserLandingTemplate mapProvider={mapProvider} />
     </>
   );
 }
