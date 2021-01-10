19f22977fa2a0217c60a9a28b58486316bd1b527
Martin Sedláček <martinsedlacek7@gmail.com>
2021-01-10 12:15:47 +0100

    testimonialboxgrid warnings fixed

--- frontend/src/molecules/TestimonialBoxGrid.js
+++ frontend/src/molecules/TestimonialBoxGrid.js
@@ -1,40 +1,7 @@
 import React from "react";
 import { TestimonialBox } from "src/atoms/";
-import { gql, useQuery } from "@apollo/client";
-import { useAuth } from "src/utils/auth";
-
-const RATINGS_QUERY = gql`
-  query getOrganizationRatings($id: Int!) {
-    organization(user_id: $id) {
-      ratings {
-        id
-        sportsman {
-          firstname
-          lastname
-          profile_photo {
-            url
-          }
-        }
-        text
-        stars
-      }
-    }
-  }
-`;
 
 export function TestimonialBoxGrid({ ratingsData }) {
-  /*const { user } = useAuth();
-  const ratingsFetcher = useQuery(RATINGS_QUERY, {
-    variables: { id: user.user_id },
-  });
-  const ratingsData = ratingsFetcher.data;*/
-  /*const ratings =
-    ratingsData === undefined ? undefined : ratingsData.organization.ratings;*/
-
-  //console.log(ratingsFetcher);
-  console.log("ratings data", ratingsData);
-  //console.log('haho', ratings);
-  //console.log('user_id', user.user_id);
 
   return (
     <div className="d-flex justify-content-center align-items-start flex-wrap">
